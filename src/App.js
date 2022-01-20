b04891965cff82004894ca711d1ae9b1db7bf8c9
Alex Santamaria <santamariaalex146@gmail.com>
2022-01-20 12:46:34 -0600

    able to delete movies from database

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import Header from "./components/Header";
 import Movies from "./components/Movies";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AddMovie from "./components/AddMovie";
 
 function App() {
   const [showAddMovie, setShowAddMovie] = useState(false);
 
   const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const getMovies = async () => {
+      const moviesFromServer = await fetchMovies();
+      setMovies(moviesFromServer);
+    };
+
+    getMovies();
+  }, []);
+
+  //Fetch movies
+  const fetchMovies = async () => {
+    const res = await fetch("http://localhost:3000/movies");
+    const data = await res.json();
+
+    return data;
+  };
 
   //Add movies function
   const addMovie = (movie) => {
@@ -16,7 +33,11 @@ function App() {
   };
 
   //delete movies function
-  const deleteMovie = (id) => {
+  const deleteMovie = async (id) => {
+    await fetch(`http://localhost:3000/movies/${id}`, {
+      method: "DELETE",
+    });
+
     setMovies(movies.filter((movie) => movie.id !== id));
   };
 
